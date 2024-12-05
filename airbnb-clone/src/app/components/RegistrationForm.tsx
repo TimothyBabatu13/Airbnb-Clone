f48ed87362c32b06134f83d18e09f65f1a1ae7c3
Timothy <knbtimothy@gmail.com>
2024-12-05 12:25:45 +0000

    added new package to signup/signin page

--- airbnb-clone/src/app/components/RegistrationForm.tsx
+++ airbnb-clone/src/app/components/RegistrationForm.tsx
@@ -1,7 +1,53 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { PhoneInput } from 'react-international-phone';
+import 'react-international-phone/style.css';
+
+
+interface responseType {
+    status: string,
+    country: string,
+    countryCode: string,
+    region: string, 
+    regionName: string,
+    city: string,
+    zip: string,
+    lat:number,
+    lon:number,
+    timezone:string,
+    isp:string,
+    org:string,
+    as:string,
+    query:string
+}
 const RegistrationForm = () => {
+
+    const [phone, setPhone] = useState('');
+    const [location, setLocation] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchCountryCode = async () : Promise<string> => {
+            const api = await fetch('http://ip-api.com/json');
+            const res: responseType = await api.json()
+            const response = res.countryCode
+            return response as string
+        }
+        const res = fetchCountryCode();
+        res.then(res => setLocation(res))
+    } , [])
+
   return (
-    <div>RegistrationForm</div>
+    <div>
+        {
+            location && (
+            <PhoneInput
+                defaultCountry={location.toLowerCase()}
+                value={phone}
+                onChange={(phone) => setPhone(phone)}
+            />)
+        }
+        
+    </div>
   )
 }
 
