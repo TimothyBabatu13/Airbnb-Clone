4e4c24e49fcc8f85e34af0e28ea82ae8c8c2f6e4
Timothy <knbtimothy@gmail.com>
2024-12-05 12:36:22 +0000

    added more functionalities to signup/signin page

--- airbnb-clone/src/app/components/UserRegistration.tsx
+++ airbnb-clone/src/app/components/UserRegistration.tsx
@@ -3,18 +3,21 @@
 import { cn } from "@/lib/utils";
 import { X } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import {  useSearchParams, useRouter } from "next/navigation";
 import RegistrationForm from "./RegistrationForm";
 
 const UserRegistration = () => {
 
+  const router = useRouter();
   const searchParams = useSearchParams()
   
   const result = searchParams.get('signup') === 'true' ? true : false
 
   return (
-   <div className= {cn("fixed  inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 ", result ? 'block' : 'hidden')}>
-      <div className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
+   <div onClick={()=> console.log('')} className= {cn("fixed  inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 ", result ? 'block' : 'hidden')}>
+      <div onClick={(e)=>{
+        e.stopPropagation()
+      }} className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
         <div className="flex items-center justify-between h-16 border-b">
           <Link href={'/'}>
             <X className="h-4 w-4" />
@@ -25,6 +28,7 @@ const UserRegistration = () => {
         <div className="py-6">
           <h3 className="mt-[25px] mb-6 text-[22px] font-medium">Welcome to Airbnb</h3>
           <RegistrationForm />
+          
         </div>
       </div>
     </div>
