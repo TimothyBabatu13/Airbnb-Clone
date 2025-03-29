b107260bfb8e11a8b0d544c76e8b38ac59d43e6a
Timothy <knbtimothy@gmail.com>
2025-03-29 12:28:14 +0000

    removed clerk]

--- airbnb-clone/src/app/layout.tsx
+++ airbnb-clone/src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+// import {
+//   ClerkProvider,
+// } from '@clerk/nextjs'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased mx-10`}
@@ -39,6 +39,6 @@ export default function RootLayout({
           <Toaster />
         </body>
       </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   );
 }
